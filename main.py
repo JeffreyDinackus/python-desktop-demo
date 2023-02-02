4c6deb29533faa558ab043cf548a98dc28a0851d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-02 15:25:25 -0500

    new version for main file, better structure coming soon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#VERSION1 : setting everything up in global scope
+"""
 #imports the componenets we need
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 #this makes the program be able to proces command line arguements
 import sys
@@ -8,8 +10,33 @@ import sys
 app = QApplication(sys.argv)
 
 #by default windows/widgets are hidden
-window = QWidget()
-window.show()
+window = QMainWindow()
+window.setWindowTitle("Desktop Python")
 
+button = QPushButton()
+button.setText("press me")
+
+window.setCentralWidget(button)
+
+window.show()
 #the exec script starts the event loop, if we run our application, and click a button, it is the even loop that responds. You can type it with the _ after the last letter so it would be exec_() but it is depreciated. It is from older versions of python. 
+app.exec()
+
+"""
+
+
+#VERSION 2 : setting up a seperate class
+
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton 
+class ButtonHolder(QMainWindow):
+    def __init__ (self):
+        super().__init__()
+        self.setWindowTitle("Button Holder App")
+        button = QPushButton("Press Me!")
+        #Set up the button as our central widget
+        self.setCentralWidget(button)
+app = QApplication(sys.argv)
+window = ButtonHolder()
+window.show() 
 app.exec()
