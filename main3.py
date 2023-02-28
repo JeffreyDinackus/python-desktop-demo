889018db20414e412db21e2c400d1ccdbd4d4847
JeffreyDinackus <z3L9m@protonmail.com>
2023-02-27 22:37:57 -0500

    new code in main3.py shows state of a pushed button

---
+++ main3.py
@@ -0,0 +1,25 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+#this makes the program be able to proces command line arguements
+import sys
+
+def button_clicked(data):
+    print("You clicked the button, didn't you? : ", data)
+
+#the application is a wrapper that executes the application and waits for things to happen in the app, like pressing a button. 
+app = QApplication(sys.argv)
+
+#by default windows/widgets are hidden
+window = QMainWindow()
+window.setWindowTitle("Desktop Python")
+
+button = QPushButton("press me")
+button.setCheckable(True)
+
+window.setCentralWidget(button)
+
+button.clicked.connect(button_clicked)
+
+window.show()
+#the exec script starts the event loop, if we run our application, and click a button, it is the even loop that responds. You can type it with the _ after the last letter so it would be exec_() but it is depreciated. It is from older versions of python. 
+app.exec()
