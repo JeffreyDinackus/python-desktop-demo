1d641c9076ab070058a7b52e1186ecc5ed0a2bf5
JeffreyDinackus <z3L9m@protonmail.com>
2023-02-27 22:40:58 -0500

    neat slider added in main3

--- main3.py
+++ main3.py
@@ -1,3 +1,5 @@
+"""
+
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 #this makes the program be able to proces command line arguements
@@ -22,4 +24,26 @@ button.clicked.connect(button_clicked)
 
 window.show()
 #the exec script starts the event loop, if we run our application, and click a button, it is the even loop that responds. You can type it with the _ after the last letter so it would be exec_() but it is depreciated. It is from older versions of python. 
+app.exec()
+
+"""
+
+#VERSION3 : CAPTURE VALUE FROM A SLIDER - OTHER EXAMPLE
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QSlider
+
+#The slot : responds when something happens
+def respond_to_slider(data):
+        print("slider moved to : ", data)
+
+app = QApplication()
+slider = QSlider(Qt.Horizontal)
+slider.setMinimum(1)
+slider.setMaximum(100)
+slider.setValue(25)
+
+#You just do the connection. The Qt system takes care of
+#  passing the data from the signal to the slot.
+slider.valueChanged.connect(respond_to_slider)
+slider.show()
 app.exec()
